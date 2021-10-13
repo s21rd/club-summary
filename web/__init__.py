a5217dac4976ccdf503c0caa2f1f2df13eed4686
s21rd <77969436+s21rd@users.noreply.github.com>
2021-10-14 00:19:49 +0900

    sqlの設定変更

--- web/__init__.py
+++ web/__init__.py
@@ -1,8 +1,10 @@
 import os
+from urllib.parse import quote_plus
 
 from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import pyodbc
 
 load_dotenv()
 
@@ -16,10 +18,15 @@ DB_USER = os.environ["DB_USERNAME"]
 DB_PASSWORD = os.environ["DB_PASSWORD"]
 DB_HOST = os.environ["DB_HOST"]
 DB_DATABASE = os.environ["DB_DATABASE"]
+DB_PORT = os.environ["DB_PORT"]
+
+params = quote_plus(
+    f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER=tcp:{DB_HOST},{DB_PORT};DATABASE={DB_DATABASE};UID={DB_USER};PWD={DB_PASSWORD};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+)
 
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}?charset=utf8"
+] = f"mysql+pyodbc:///?odbc_connect={params}?charset=utf8"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
