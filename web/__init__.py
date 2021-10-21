073852857ec1845273358a3dbd50693f37ff23bc
s21rd <77969436+s21rd@users.noreply.github.com>
2021-10-21 10:47:26 +0900

    fix

--- web/__init__.py
+++ web/__init__.py
@@ -4,7 +4,6 @@ from urllib.parse import quote_plus
 from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import pyodbc
 
 load_dotenv()
 
@@ -20,14 +19,12 @@ DB_HOST = os.environ["DB_HOST"]
 DB_DATABASE = os.environ["DB_DATABASE"]
 DB_PORT = os.environ["DB_PORT"]
 
-params = quote_plus(
-    f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER=tcp:{DB_HOST},{DB_PORT};DATABASE={DB_DATABASE};UID={DB_USER};PWD={DB_PASSWORD};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
-)
-
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = f"mysql+pyodbc:///?odbc_connect={params}?charset=utf8"
+] = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}?charset=utf8"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
 
 db = SQLAlchemy(app)
 
